72de381bc076c1e1e1d702a3e16cf6a7b7944aaf
juliana-castro8 <124001861+juliana-castro8@users.noreply.github.com>
2026-04-08 10:54:04 -0500

    connected to localhost

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.use(express.urlencoded({extended: true}));
 app.get('/test', (req, res) => {
     res.render('test');
 });
+app.get('/', (req, res) => {
+    res.render('home');
+});
+app.get('/leaderboard', (req, res) => {
+    res.render('leaderboard');
+});
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
