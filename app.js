421fa1bd0d0efde4a3052a444137e36d2af94ce7
juliana-castro8 <124001861+juliana-castro8@users.noreply.github.com>
2026-04-08 13:21:22 -0500

    created a partial for the navigation bar and fixed home page

--- app.js
+++ app.js
@@ -8,17 +8,29 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended: true}));
 
 app.get('/test', (req, res) => {
-    res.render('test');
+    res.render('pages/test');
 });
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('pages/home');
 });
 app.get('/leaderboard', (req, res) => {
-    res.render('leaderboard');
+    res.render('pages/leaderboard');
 });
 app.get('/login', (req, res) => {
-    res.render('login');
+    res.render('pages/login');
 });
+app.get('/quiz', (req, res) => {
+    res.render('pages/quiz');
+});
+app.get('/sanAntonio', (req, res) => {
+    res.render('pages/sanAntonio');
+});
+app.get('/suggest', (req, res) => {
+    res.render('pages/suggest');
+});
+app.get('/weeklyQuiz', (req, res) => {
+    res.render('pages/weeklyQuiz');
+}); 
 
 
 app.listen(3000, () => {
